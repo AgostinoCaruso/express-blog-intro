19d451f16454b502ace62d0df983fbeee057767f
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-12-02 23:46:50 +0100

    filter per img o per titolo che torna un oggetto

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ filtrare i dati sulla base di parametri in query string
  */
 
 //server
-const express = require("express");//associare a variabile l'oggetto express
+const express = require("express"); //associare a variabile l'oggetto express
 const server = express();
 const PORT = 3000;
 //other var
@@ -19,56 +19,74 @@ server.use(express.static("public"));
 
 //qua partendo dal port 3000 ho l'inizio del server
 server.get("/", (req, res) => {
-    res.send("<h1>Server del mio blog</h1>");
-    //res.json(lista);
+  res.send("<h1>Server del mio blog</h1>");
 });
 
 //qua dall'inizio aggiungo un path diverso che mi porta da un altra parte
 server.get("/bacheca", (req, res) => {
-    const objLista = {
-        lista,
-        num: lista.length
-    };
-    res.json(objLista);
+
+  const objLista = {
+    lista,
+    num: lista.length,
+  };
+  
+  res.json(objLista);
 });
 
-//filtro per ogni index dentro a bacheca
-server.get("/bacheca/:index", (req, res) => {
-    const index = parseInt(req.params.index);
-    
-    if (index >= 0 && index < lista.length) {
-        res.json(lista[index]);
-    } else {
-        res.status(404).send("Post non trovato.");
-    }
+//ritorna l'oggetto tramite titolo
+server.get("/bacheca/obj/:titolo", (req, res) => {
+  let titolo = req.params.titolo;
+
+  // Trova un post che include la stringa passata
+  const post = lista.find((post) =>
+    post.titolo.toLowerCase().includes(titolo.toLowerCase())
+  );
+
+  if (post) {
+
+    res.json(post); // Usa sendFile per inviare l'immagine
+  } else {
+
+    res.status(404).send("Oggetto non trovato");
+  }
 });
 
-//ti printa tutte le img dentro a lista
+//ritorna l'oggetto tramite index
+server.get("/bacheca/index/:index", (req, res) => {
+  const index = parseInt(req.params.index);
+
+  if (index >= 0 && index < lista.length) {
+    res.json(lista[index]);
+  } else {
+    res.status(404).send("Post per indice non trovato.");
+  }
+});
+
+//ti printa tutti i titoli delle img dentro a lista
 server.get("/img", (req, res) => {
-
-    res.json(lista.map(post => post.img));
+  res.json(lista.map((post) => post.img));
 });
 
-//se scrivi le iniziali dell img desiderata poi te la completa
+//ti printa a schermo l'immagine del titolo collegata
 server.get("/img/:titolo", (req, res) => {
-    let titolo = req.params.titolo;
+  let titolo = req.params.titolo;
 
-    // Trova un post che include la stringa passata
-    const post = lista.find(post => post.titolo.toLowerCase().includes(titolo.toLowerCase()));
+  // Trova un post che include la stringa passata
+  const post = lista.find((post) =>
+    post.titolo.toLowerCase().includes(titolo.toLowerCase())
+  );
 
-    if (post) {
-        // Assicurati che il percorso dell'immagine sia corretto
-        const imgPath = post.img;
-        res.sendFile(imgPath, { root: './public' }); // Usa sendFile per inviare l'immagine
-    } else {
-        res.status(404).send("Immagine non trovata"); // Risposta per il caso in cui non trovi il post
-    }
+  if (post) {
+
+    res.sendFile(post.img, { root: "./public" }); // Usa sendFile per inviare l'immagine
+  } else {
+
+    res.status(404).send("Immagine non trovata"); 
+  }
 });
-
-
 
 //qua sto in ascolto per la porta 3000
 server.listen(PORT, () => {
-    console.log("Server del mio blog");
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log("Server del mio blog");
+  console.log(`Server running on http://localhost:${PORT}`);
 });
